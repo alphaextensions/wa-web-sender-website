8bf2303ac8571d4dcb0551b62d798f60a88c3f9b
Jay Shah <jay@almaconnect.com>
2021-04-18 02:20:06 +0530

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GA_TRACKING_ID
       }
     }
   ]
