8ffe1f967bfda09f401dc156f562014fbb09f995
harshulshah <harshulshah2611@gmail.com>
2023-01-24 23:09:29 +0530

    updated few componenets

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,27 +21,38 @@ import service2 from '../svg/undraw_Spreadsheet_re_cn18.png';
 import service1 from '../svg/undraw_text_field_htlv.png';
 import screenshot from '../svg/screenshot.png';
 
-
 export default () => (
   <Layout>
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            #1 Extension for your business
+            Best Extension for your business
           </h1>
-          {/* <p className="text-xl lg:text-2xl mt-6 font-light">
-            Broadcast messages to users simply & efficiently
-          </p> */}
           <p className="mt-8 md:mt-12">
-            <Button size="lg" link="https://chrome.google.com/webstore/detail/wa-web-sender/klfaghfflijdgoljefdlofkoinndmpia?src=website">Download Now</Button>
+            <a
+              href="https://chrome.google.com/webstore/detail/wa-web-sender/klfaghfflijdgoljefdlofkoinndmpia?src=website"
+              target="_blank"
+              style={{
+                backgroundColor: '#009a88',
+                color: '#fff',
+                paddingTop: '1rem',
+                paddingBottom: '1rem',
+                paddingLeft: "3rem",
+                paddingRight: "3rem",
+                borderRadius:"0.25rem",
+              }}
+            >
+              Download Now
+            </a>
           </p>
-          <p className="text-2xl mt-8" style={{marginRight: '24px'}}>Prime Sender is the best google chrome extension to enhance your business. It is the currently the highest rated and the best tool on the chrome webstore. Download now! <br/><br/>
+          <p className="text-2xl mt-8" style={{marginRight: '24px',fontWeight:"100",fontSize:"1.4rem"}}>Best productivity and communication tool for business. Prime Sender is the best google chrome extension to enhance your business. It is the currently the highest rated and the best tool on the chrome webstore.<br/><br/></p>
 
-            Lifetime Offer : Basic Features FREE FOREVER!</p>
+         <p className='text-2xl'>Lifetime Offer : Basic Features FREE FOREVER!</p>
+          {/*<p className="mt-4 text-gray-600">Sed fermentum felis ut cursu</p>*/}
         </div>
         <div className="lg:w-1/2">
-          <img src={screenshot} style={{width: '100%'}}/>
+          <img src={screenshot} style={{ width: '100%' }} />
         </div>
       </div>
     </section>
@@ -51,17 +62,30 @@ export default () => (
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Customisation</p>
+              <p className="font-semibold text-xl">Report</p>
+              <p className="mt-4">
+                Get a detailed report of your campaigns to improve sales and utilize Prime Sender to
+                the fullest
+                {/* Get a detailed report of your campaigns and promotions to improve sales and utilize Web Sender to the fullest */}
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Add attachment</p>
+              <p className="font-semibold text-xl">File Attachment</p>
+              <p className="mt-4">
+                You can attach and send images, document, videos, etc along with your message to
+                users
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Download delivery report</p>
+              <p className="font-semibold text-xl">Customise your message</p>
+              <p className="mt-4">
+                You can customise your message according to the customer with their name, email,
+                order number,etc
+              </p>
             </Card>
           </div>
         </div>
@@ -70,17 +94,28 @@ export default () => (
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Extremely swift and quick customer service</p>
+              <p className="font-semibold text-xl">Schedule your message</p>
+              <p className="mt-4">
+                You can schedule at what time to send your messages to users and your messages would
+                be sent automatically at the set time
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Schedule</p>
+              <p className="font-semibold text-xl">Quick Response</p>
+              <p className="mt-4">
+                You can respond to your customers quickly, with pre-saved responses
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Respond to your customers with quick replies</p>
+              <p className="font-semibold text-xl">Active support</p>
+              <p className="mt-4">
+                You can click on 'Live Support' on the extension to get your queries resolved. We
+                also provide call and video support.
+              </p>
             </Card>
           </div>
         </div>
@@ -89,62 +124,88 @@ export default () => (
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Business Chat Link for your website</p>
+              <p className="font-semibold text-xl">Upload excel of numbers</p>
+              <p className="mt-4">
+                You can directly upload an excel sheet of numbers you want to send your message to
+                without having to type or copy paste
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">Real time update via notifications</p>
+              <p className="font-semibold text-xl">Download contacts from group</p>
+              <p className="mt-4">
+                You can get a detailed download of all the contacts from a group including the ones
+                you have not saved
+              </p>
             </Card>
           </div>
           <div className="flex-1 px-3">
             <Card className="mb-8">
-              <p className="font-semibold text-xl">No Software Installation Needed</p>
+              <p className="font-semibold text-xl">Message Formatting</p>
+              <p className="mt-4">
+                You can format your message - BOLD, italic, strikethrough and Emojis
+              </p>
             </Card>
           </div>
+        </div>
+      </div>
+      <div className="container mx-auto text-center">
+        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Real Time excel preview</p>
+              <p className="mt-4">Get a real time preview of uploaded CSV of numbers</p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3">
+            <Card className="mb-8">
+              <p className="font-semibold text-xl">Real time notification</p>
+              <p className="mt-4">Get real time update via notifications</p>
+            </Card>
+          </div>
+          <div className="flex-1 px-3"></div>
         </div>
       </div>
     </section>
-    {/* <SplitSection
+    <SplitSection
       id="services"
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">Send broadcast messages in a few clicks</h3>
+          <h3 className="text-3xl font-semibold leading-tight">
+            Send broadcast messages in a few clicks
+          </h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Prime Sender lets you broadcast messages to users according to your customization.
-            This can be done in a few clicks and the messages will be send in a few seconds.
+            Prime Sender lets you broadcast messages to users according to your customization. This
+            can be done in a few clicks and the messages will be send in a few seconds.
           </p>
         </div>
       }
-      secondarySlot={<img src={service1} alt="WS" />}
+      secondarySlot={<img src={service1} alt="PS" />}
     />
     <SplitSection
       reverseOrder
       primarySlot={
         <div className="lg:pl-32 xl:pl-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Import CSV
-          </h3>
+          <h3 className="text-3xl font-semibold leading-tight">Import CSV</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
             Ability to import csv of numbers.
           </p>
         </div>
       }
-      secondarySlot={<img src={service2} alt="WS" />}
+      secondarySlot={<img src={service2} alt="PS" />}
     />
     <SplitSection
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
-          <h3 className="text-3xl font-semibold leading-tight">
-            Add Attachment
-          </h3>
+          <h3 className="text-3xl font-semibold leading-tight">Add Attachment</h3>
           <p className="mt-8 text-xl font-light leading-relaxed">
-            Add and send attachments including photos, videos, contacts, document,etc.
-            This way you can send promotional posters to you customers and be connected.
+            Add and send attachments including photos, videos, contacts, document,etc. This way you
+            can send promotional posters to you customers and be connected.
           </p>
         </div>
       }
-      secondarySlot={<img src={service3} alt="WS" />}
-    /> */}
+      secondarySlot={<img src={service3} alt="PS" />}
+    />
   </Layout>
 );
