2fdeeb0d7e9de00c2e2f8dbd21e5fec856d8c4eb
Meet Bhimani <bhimanimeet55555@gmail.com>
2023-04-22 11:37:54 +0530

    updated attachment description

--- src/components/howToUseCard.js
+++ src/components/howToUseCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../css/howToUseCard.css'
 
-const Ucard = ({ imgSrc, title, description }) => {
+const Ucard = ({ imgSrc, title, description,detailedDescription }) => {
   return (
     <div className='ucard'>
       <img src={imgSrc} alt={title} />
       <h3 style={{fontWeight:700}}>{title}</h3>
       <div>
         <p>{description}</p>
+        <p className='detailedDesc'>{detailedDescription}</p>
       </div>
     </div>
   );
